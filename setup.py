37d564dfa6e47c11aeef095d4a3faa186eb15456
Norman Radtke <norman.radtke@gmail.com>
2019-09-25 10:28:57 +0200

    Add first files

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='mypythonstuff',
+    version='1.0.0',
+    url='https://github.com/splattater/mypythonstuff',
+    author='Norman Radtke',
+    author_email='norman.radtke@gmail.com',
+    description='things I need from time to time ...',
+    packages=find_packages(),
+    install_requires=[],
+)
